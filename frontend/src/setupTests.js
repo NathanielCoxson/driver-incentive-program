6d40a6c5ffd9ade31c60b374a82e431e6a295665
Nathaniel Coxson <ncoxson@clemson.edu>
2023-09-23 21:28:58 -0400

    #315 and #484, added a line to react readme, installed dotenv to use an environment variables file, installed react router, and moved App files to a new Components folder for better organization.

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -2,4 +2,5 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+// How to test React apps can be found here: https://jestjs.io/docs/tutorial-react
 import '@testing-library/jest-dom';
