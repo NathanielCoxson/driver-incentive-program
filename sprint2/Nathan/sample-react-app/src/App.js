12ead9da56a143e6497287179acde64306106142
Nathaniel Coxson <ncoxson@clemson.edu>
2023-09-24 00:26:18 -0400

    #469, added a sample react app for Nathan.

---
+++ sprint2/Nathan/sample-react-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import tigerPaw from './tigerPaw.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sample React App</h1>
+        <img src={tigerPaw} alt="logo" className="App-logo"></img>
+      </header>
+    </div>
+  );
+}
+
+export default App;
