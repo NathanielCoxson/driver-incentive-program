cd57d0b17cbbb061100e9aff4b167a9fc7616acb
NathanCoxson <nathancoxson2739@gmail.com>
2023-09-26 15:10:46 -0400

    Removed old frontend folder.

--- frontend/src/setupTests.js
+++
@@ -1,6 +0,0 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-// How to test React apps can be found here: https://jestjs.io/docs/tutorial-react
-import '@testing-library/jest-dom';
