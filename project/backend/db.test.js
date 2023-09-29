40fcfd719c0211a9393b6695912eba292dccf8e3
Nathaniel Coxson <ncoxson@clemson.edu>
2023-09-29 17:25:48 -0400

    Added a test file for the server db file and added the supertest package to make requests within the tests.

---
+++ project/backend/db.test.js
@@ -0,0 +1,10 @@
+const db = require('./db');
+const request = require('supertest');
+const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+
+describe('GET /about', () => {
+    it ('should return 200', async () => {
+        const response = (await request(baseUrl).get('/about'));
+        expect(response.statusCode).toBe(200);
+    });
+});
