5d4d7372cf50cadbee5e0462e6b721e5b9ec5077
David Croft <dcroft@clemson.edu>
2023-10-03 01:44:43 +0000

    Updated DBConnection.js

--- project/testing/DBConnection.js
+++ project/testing/DBConnection.js
@@ -1,7 +1,18 @@
-function Connect() {
-  return (
-    
-  );
+// Function to make the HTTP request
+function fetchData() {
+  // URL of the API endpoint
+  const apiUrl = "http://34.225.199.196:3000/about";
+          
+  // Make the GET request
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle the JSON response data here
+      return data
+    })
+    .catch((error) => {
+      return "Error fetching data"
+    });
 }
 
-export default Connect;
+export default fetchData;
