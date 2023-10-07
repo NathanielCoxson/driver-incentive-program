cb1ba9e47563aafbc1e8c6d8d7d23ea0fe036927
Nathaniel Coxson <ncoxson@clemson.edu>
2023-10-07 15:01:39 -0400

    Updated api test file to have the new url and re-added supertest to dependencies.

--- project/backend/db.test.js
+++ project/backend/db.test.js
@@ -1,10 +1,10 @@
 const db = require('./db');
 const request = require('supertest');
-const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+const baseUrl = process.env.BASE_URL || "http://localhost:3000/api";
 
-describe('GET /about', () => {
+describe('GET /api/about', () => {
     it ('should return 200', async () => {
-        const response = (await request(baseUrl).get('/about'));
+        const response = await request(baseUrl).get('/about');
         expect(response.statusCode).toBe(200);
     });
 });
