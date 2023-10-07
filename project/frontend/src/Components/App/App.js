11a8251f984546dffa8337b4c410d344e2485734
lanttyc <lanttyc@g.clemson.edu>
2023-10-07 16:11:47 -0400

    Add BreadCrum

--- project/frontend/src/Components/App/App.js
+++ project/frontend/src/Components/App/App.js
@@ -8,6 +8,7 @@ import Rewards from '../Rewards/Rewards';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import Register from '../Register/Register';
+import BreadCrum from '../BreadCrum/BreadCrum';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
+        <BreadCrum />
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/catalog" element={<Catalog />} />
--- project/frontend/src/Components/BreadCrum/BreadCrum.js
+++ project/frontend/src/Components/BreadCrum/BreadCrum.js
@@ -1,24 +1,31 @@
-import './Breadcrumb.css'
-import { Link } from 'react-router-dom';
+import './BreadCrum.css';
+import { useEffect, useState } from 'react';
 
-// JavaScript to update breadcrumb
-function updateBreadcrumb() {
-    const breadcrumb = document.getElementById('breadcrumb');
-    const path = window.location.pathname.split('/').filter(Boolean); // Split the URL path
-    let breadcrumbHTML = '';
+function BreadCrumb() {
+    useEffect(() => {
+        // Breadcrumb update logic
+        const breadcrumb = document.getElementById('breadcrumb');
+        const path = window.location.pathname.split('/').filter(Boolean); // Split the URL path
+        let breadcrumbHTML = '';
 
-    // Create breadcrumb links
-    for (let i = 0; i < path.length; i++) {
-        breadcrumbHTML += '<a href="/' + path.slice(0, i + 1).join('/') + '">' + path[i] + '</a>';
-        if (i < path.length - 1) {
-            breadcrumbHTML += '<span>/</span>';
+        // Create breadcrumb links
+        for (let i = 0; i < path.length; i++) {
+            breadcrumbHTML += '<a href="/' + path.slice(0, i + 1).join('/') + '">' + path[i] + '</a>';
+            if (i < path.length - 1) {
+                breadcrumbHTML += '<span>/</span>';
+            }
         }
-    }
 
-    breadcrumb.innerHTML = breadcrumbHTML;
+        if (breadcrumb) {
+            breadcrumb.innerHTML = breadcrumbHTML;
+        }
+    }, []);
+
+    return (
+        <div>
+            <div id="breadcrumb"></div>
+        </div>
+    );
 }
 
-// Call the updateBreadcrumb function when the page loads
-window.onload = function () {
-    updateBreadcrumb();
-};
+export default BreadCrumb;
