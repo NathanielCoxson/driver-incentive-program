361ea6f1cd168004b9b21ca9d5281a852cf8ee89
lanttyc <lanttyc@g.clemson.edu>
2023-10-06 20:18:06 -0400

    Added a signup button and Remodify the Reward page

--- project/frontend/src/Components/Rewards/Rewards.js
+++ project/frontend/src/Components/Rewards/Rewards.js
@@ -1,5 +1,12 @@
 function Rewards() {
-    return (<h1>rewards page works</h1>)
+    return (<main>
+        <section className="hero">
+            <h2>Reward</h2>
+            <p>
+                Here are the rewards that the driver has exchange out.
+            </p>
+        </section>
+    </main>)
 }
 
 export default Rewards;
