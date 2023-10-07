3cc6701e47d9286d392c9897f54726627e6391ad
lanttyc <lanttyc@g.clemson.edu>
2023-10-06 23:02:47 -0400

    remove useless lines

--- project/frontend/src/Components/Home/Home.js
+++ project/frontend/src/Components/Home/Home.js
@@ -4,7 +4,6 @@ function Home() {
             <h2>Drive Safely and Get Rewarded!</h2>
             <p>Join our program and earn rewards for your good driving behavior.</p>
             <a href="login" className="cta-button">Login</a>
-            <a href="signin.html" className="sign-in-button">Sign In</a>
         </section>
     </main>)
 }
