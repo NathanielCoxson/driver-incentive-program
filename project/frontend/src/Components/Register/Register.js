cafa8ef8f9f276d997ebe69af8189f6d9bdc6223
lanttyc <lanttyc@g.clemson.edu>
2023-10-10 13:06:38 -0400

    Change format of passwdReq

--- project/frontend/src/Components/Register/Register.js
+++ project/frontend/src/Components/Register/Register.js
@@ -142,11 +142,11 @@ function Register() {
                     <div className="password-requirements">
                         <p>Password requirements:</p>
                         <ul>
-                            <li>At least eight characters long</li>
-                            <li>Contain one uppercase letter</li>
-                            <li>Contain one lowercase letter</li>
-                            <li>Contain one number</li>
-                            <li>Contain one special character</li>
+                            <li>&#9642; At least eight characters long</li>
+                            <li>&#9642; Contain one uppercase letter</li>
+                            <li>&#9642; Contain one lowercase letter</li>
+                            <li>&#9642; Contain one number</li>
+                            <li>&#9642; Contain one special character</li>
                         </ul>
                     </div>
 
