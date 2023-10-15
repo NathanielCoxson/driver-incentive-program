bc59a2ded278c2f24d89b419f19f44a40c98a6fc
lanttyc <lanttyc@g.clemson.edu>
2023-10-15 15:25:04 -0400

    Edit side_bar style

--- project/frontend/src/Components/Driver_Dashboard/Driver_Dashboard.js
+++ project/frontend/src/Components/Driver_Dashboard/Driver_Dashboard.js
@@ -4,19 +4,21 @@ import { Link } from 'react-router-dom';
 function DriverDashboard() {
     return (
         <main>
-            <div class="sidebar">
-                <ul>
-                    <li><a href="driver_dashboard.html">Dashboard Home</a></li>
-                    <li><a href="profile.html">Profile</a></li>
-                    <li><a href="sponsor-organization.html">Sponsor Organization</a></li>
-                    <li><a href="catalog.html">Catalog</a></li>
-                </ul>
-            </div>
             <section className="hero">
                 <h2>Welcome to Your Dashboard</h2>
                 <p>Welcome to your driver dashboard. Here you can access various features and information.</p>
                 <a href="#" className="cta-button">Explore</a>
             </section>
+            <div className="sidebar-container">
+                <div className="sidebar">
+                    <ul>
+                        <li><a href="driver_dashboard.html">Dashboard Home</a></li>
+                        <li><a href="profile.html">Profile</a></li>
+                        <li><a href="sponsor-organization.html">Sponsor Organization</a></li>
+                        <li><a href="catalog.html">Catalog</a></li>
+                    </ul>
+                </div>
+            </div>
         </main>
     );
 }
