8d44b77b771c56930916d541d1681b9f8d8aa7bc
lanttyc <lanttyc@g.clemson.edu>
2023-10-16 12:02:15 -0400

    Add routing in side-bar

--- project/frontend/src/Components/Sidebar/Driver_Sidebar.js
+++ project/frontend/src/Components/Sidebar/Driver_Sidebar.js
@@ -8,8 +8,8 @@ function DriverSidebar() {
                 <li><Link to="/driver_dashboard">Driver Home</Link></li>
                 <li><Link to="/profile">Driver Profile</Link></li>
                 <li><Link to="/driver_point">Driver Point</Link></li>
-                <li><Link to="/sponsor-organization">Sponsor Organization</Link></li>
-                <li><Link to="/catalog">Driver Catalog</Link></li>
+                <li><Link to="/driver_sponsor_organization">Sponsor Organization</Link></li>
+                <li><Link to="/driver_catalog">Driver Catalog</Link></li>
             </ul>
         </div>
     );
