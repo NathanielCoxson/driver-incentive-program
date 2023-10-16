bba881c684bcdfad90a771f890d99284dde192f3
lanttyc <lanttyc@g.clemson.edu>
2023-10-16 17:54:53 -0400

    Add Admin sidebar

---
+++ project/frontend/src/Components/Sidebar/Admin_Sidebar.js
@@ -0,0 +1,20 @@
+import "./Driver_Sidebar.css"
+import { Link } from 'react-router-dom';
+
+function AdminSidebar() {
+    return (
+        <div className="sidebar">
+            <ul>
+                <li><Link to="/admin_dashboard">Admin Home</Link></li>
+                <li><Link to="/profile">Admin Profile</Link></li>
+                <li><Link to="/admin_add_user">Add User</Link></li>                
+                <li><Link to="/admin_dashboard/admin_messagebox">Message Box</Link></li>
+            </ul>
+        </div>
+    );
+}
+
+export default AdminSidebar;
+
+
+
