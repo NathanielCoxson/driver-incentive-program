26cd2fdc90440fd1b144ef85c4798aed68454e56
lanttyc <lanttyc@g.clemson.edu>
2023-10-16 13:35:56 -0400

    add sponsor sidebar

---
+++ project/frontend/src/Components/Sidebar/Sponsor_sidebar.js
@@ -0,0 +1,17 @@
+import "./Driver_Sidebar.css"
+import { Link } from 'react-router-dom';
+
+function SponsorSidebar() {
+    return (
+        <div className="sidebar">
+            <ul>
+                <li><Link to="/sponsor_dashboard">Sponsor Home</Link></li>
+                <li><Link to="/profile">Sponsor Profile</Link></li>
+                <li><Link to="/sponsor_organization">Sponsor Organization</Link></li>
+                <li><Link to="/sponsor_catalog">Sponsor Catalog</Link></li>
+            </ul>
+        </div>
+    );
+}
+
+export default SponsorSidebar;
