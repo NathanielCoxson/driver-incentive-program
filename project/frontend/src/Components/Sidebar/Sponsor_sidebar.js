b8fcc65964e71e5ae3e2d59628cb8e3b97c24ad4
lanttyc <lanttyc@g.clemson.edu>
2023-10-16 13:58:48 -0400

    Update Sidebar error

--- project/frontend/src/Components/Sidebar/Sponsor_sidebar.js
+++ project/frontend/src/Components/Sidebar/Sponsor_sidebar.js
@@ -1,14 +1,15 @@
-import "./Sponsor_Sidebar.css"
+import "./Driver_Sidebar.css"
 import { Link } from 'react-router-dom';
 
 function SponsorSidebar() {
     return (
         <div className="sidebar">
             <ul>
-                <li><Link to="/sponsor_dashboard">Sponsor Home</Link></li>
-                <li><Link to="/profile">Sponsor Profile</Link></li>
-                <li><Link to="/sponsor_organization">Sponsor Organization</Link></li>
-                <li><Link to="/sponsor_catalog">Sponsor Catalog</Link></li>
+                <li><Link to="/driver_dashboard">Driver Home</Link></li>
+                <li><Link to="/profile">Driver Profile</Link></li>
+                <li><Link to="/driver_point">Driver Point</Link></li>
+                <li><Link to="/driver_sponsor_organization">Driver's Sponsor Organization</Link></li>
+                <li><Link to="/driver_catalog">Driver Catalog</Link></li>
             </ul>
         </div>
     );
