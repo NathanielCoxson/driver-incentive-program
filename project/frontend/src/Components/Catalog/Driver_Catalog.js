2648c4987d764365d361ae20d1a2e0525fb6e529
lanttyc <lanttyc@g.clemson.edu>
2023-10-16 12:35:25 -0400

    add Catalog

--- project/frontend/src/Components/Catalog/Driver_Catalog.js
+++ project/frontend/src/Components/Catalog/Driver_Catalog.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './driver_catalog.css';
+import './catalog.css';
 import DriverSidebar from '../Sidebar/Driver_Sidebar';
 import { Link } from 'react-router-dom';
 
