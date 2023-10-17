ebe836591cfd921d904d97754d2d60cc72af4b7c
lanttyc <lanttyc@g.clemson.edu>
2023-10-16 20:12:00 -0400

    Accept/decline Sponsor Invitation from the driver dashboard

--- project/frontend/src/Components/Dashboard/Driver_Dashboard.js
+++ project/frontend/src/Components/Dashboard/Driver_Dashboard.js
@@ -4,14 +4,40 @@ import DriverSidebar from '../Sidebar/Driver_Sidebar';
 import { Link } from 'react-router-dom';
 
 function DriverDashboard() {
+    // Simulated invitation data
+    const invitation = {
+        organizationName: 'Sample Organization', // Replace with actual organization name
+        sponsorName: 'Sponsor Name', // Replace with actual sponsor name
+    };
+
+    const acceptInvitation = () => {
+        // Handle the logic to accept the invitation
+        alert('Invitation accepted');
+    };
+
+    const declineInvitation = () => {
+        // Handle the logic to decline the invitation
+        alert('Invitation declined');
+    };
+
     return (
         <main>
             <div className="sidebar-container">
-                <DriverSidebar /> {/* Include the DriverSidebar component here */}
+                <DriverSidebar />
                 <section className="hero">
                     <h2>Welcome to Your Dashboard</h2>
-                    <p>Welcome to your driver dashboard. Here you can access various features and information.</p>             
+                    <p>Welcome to your driver dashboard. Here you can access various features and information.</p>
                     <Link to="/driver_dashboard" className="cta-button">Explore</Link>
+
+                    {/* Invitation section */}
+                    {invitation && (
+                        <div className="invitation-section">
+                            <h3>Invitation to Join {invitation.organizationName}</h3>
+                            <p>You have received an invitation from {invitation.sponsorName} to join {invitation.organizationName}.</p>
+                            <button onClick={acceptInvitation} className="cta-button">Accept</button>
+                            <button onClick={declineInvitation} className="cta-button">Decline</button>
+                        </div>
+                    )}
                 </section>
             </div>
         </main>
@@ -19,5 +45,3 @@ function DriverDashboard() {
 }
 
 export default DriverDashboard;
-
-
