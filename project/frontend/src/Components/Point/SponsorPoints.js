69367e5a4f4b3501ae7dba41e8328d340817c89c
lanttyc <lanttyc@g.clemson.edu>
2023-10-22 17:28:39 -0400

    modify sponsor point

--- project/frontend/src/Components/Point/SponsorPoints.js
+++ project/frontend/src/Components/Point/SponsorPoints.js
@@ -1,32 +1,26 @@
 import React, { useState } from 'react';
-import './DriverPoints.css';
+import './SponsorPoints.css';
 
 function SponsorPoints() {
+    const [drivers, setDrivers] = useState([
+        { name: 'Driver 1', points: 100 },
+        { name: 'Driver 2', points: 75 },
+        { name: 'Driver 3', points: 120 },
+    ]);
     const [pointsChange, setPointsChange] = useState('');
-    const [responseMessage, setResponseMessage] = useState('');
 
-    // Function to handle the form submission
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const handlePointsChange = (action, driverIndex) => {
+        const updatedDrivers = [...drivers];
+        const [pointsAction, pointsValue] = pointsChange.split(',');
 
-        // Split the pointsChange string into its components
-        const [action, points, driver] = pointsChange.split(',');
-
-        // Validate the input
-        if (!['add', 'reduce'].includes(action)) {
-            setResponseMessage('Invalid action. Use "add" or "reduce".');
-        } else if (isNaN(points) || points === '') {
-            setResponseMessage('Points must be a number.');
-        } else if (!driver) {
-            setResponseMessage('Driver name is required.');
-        } else {
-            // Example: Send a request to your server with action, points, and driver
-            // You can use an API or other communication method to perform this action
-
-            // After successful points adjustment, clear the form and display a success message
-            setPointsChange('');
-            setResponseMessage(`Points ${action}ed successfully for ${driver}.`);
+        if (action === 'add') {
+            updatedDrivers[driverIndex].points += parseInt(pointsValue, 10);
+        } else if (action === 'reduce') {
+            updatedDrivers[driverIndex].points -= parseInt(pointsValue, 10);
         }
+
+        setPointsChange('');
+        setDrivers(updatedDrivers);
     };
 
     return (
@@ -35,23 +29,22 @@ function SponsorPoints() {
             <p>Welcome to your sponsor dashboard. Here you can access various features and information.</p>
             <h2>Points Adjustment</h2>
 
-            <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                    <label htmlFor="pointsChange">Points Change (e.g., add,10,Driver Name):</label>
-                    <input
-                        type="text"
-                        id="pointsChange"
-                        name="pointsChange"
-                        value={pointsChange}
-                        onChange={(e) => setPointsChange(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit" className="cta-button">
-                    Submit Change
-                </button>
-            </form>
-            {responseMessage && <p>{responseMessage}</p>}
+            <div className="driver-list">
+                {drivers.map((driver, index) => (
+                    <div key={index} className="driver-entry">
+                        <h3>{driver.name}</h3>
+                        <p>Points: {driver.points}</p>
+                        <input
+                            type="text"
+                            value={pointsChange}
+                            onChange={(e) => setPointsChange(e.target.value)}
+                            placeholder="Enter points change (e.g., add,10 or reduce,5)"
+                        />
+                        <button onClick={() => handlePointsChange('add', index)}>Add</button>
+                        <button onClick={() => handlePointsChange('reduce', index)}>Reduce</button>
+                    </div>
+                ))}
+            </div>
         </section>
     );
 }
