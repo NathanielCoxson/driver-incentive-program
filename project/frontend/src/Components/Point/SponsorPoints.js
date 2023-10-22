cc9cdd54bb52ba2b789ab6bc1b22dd20094ca6e3
lanttyc <lanttyc@g.clemson.edu>
2023-10-22 17:00:21 -0400

    modify sponsor point

---
+++ project/frontend/src/Components/Point/SponsorPoints.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import './DriverPoints.css';
+
+function SponsorPoints() {
+
+    return (
+        <section className="hero">
+            <h2>Welcome to Your Point Dashboard</h2>
+            <p>Welcome to your driver dashboard. Here you can access various features and information.</p>
+            <h2>Points</h2>
+
+        </section>
+    );
+}
+
+export default SponsorPoints;
