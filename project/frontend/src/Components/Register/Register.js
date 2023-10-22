c526fe76a00e987d96593fb023fc6e7e67459707
lanttyc <lanttyc@g.clemson.edu>
2023-10-22 00:12:13 -0400

    change in register

--- project/frontend/src/Components/Register/Register.js
+++ project/frontend/src/Components/Register/Register.js
@@ -148,7 +148,7 @@ function Register({ role }) {
                     </div>
                 )}
                 
-                {(!role) && (
+                {(role === 'driver') && (
                     <div>
                         <label htmlFor="vehicleInfo">Vehicle Information:</label>
                         <input
