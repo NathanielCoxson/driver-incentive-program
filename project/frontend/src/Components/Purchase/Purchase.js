5040f31965bb708ad5fd6bc6ccc28cb707a10a65
lanttyc <lanttyc@g.clemson.edu>
2023-10-26 13:29:12 -0400

    Purchase

---
+++ project/frontend/src/Components/Purchase/Purchase.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+function Purchase() {
+    // Sample shopping cart data (replace with your data)
+    const shoppingCart = [
+        { id: 1, name: 'Product A', quantity: 2, costPerItem: 10 },
+        { id: 2, name: 'Product B', quantity: 3, costPerItem: 15 },
+        { id: 3, name: 'Product C', quantity: 1, costPerItem: 20 },
+    ];
+
+    // Calculate the total cost
+    const totalCost = shoppingCart.reduce((total, item) => total + item.quantity * item.costPerItem, 0);
+
+    return (
+        <main>
+            <section className="hero">
+                <h2>Purchase</h2>
+            </section>
+
+            <section className="shopping-cart">
+                <h3>Shopping Cart</h3>
+                <ul>
+                    {shoppingCart.map((item) => (
+                        <li key={item.id}>
+                            <span>{item.name} - Quantity: {item.quantity}</span>
+                            <span>Cost Per Item: ${item.costPerItem}</span>
+                            <span>Subtotal: ${item.quantity * item.costPerItem}</span>
+                        </li>
+                    ))}
+                </ul>
+
+                <div className="total-cost">
+                    <h4>Total Cost: ${totalCost}</h4>
+                </div>
+
+                <button className="cta-button">Checkout</button>
+            </section>
+        </main>
+    );
+}
+
+export default Purchase;
