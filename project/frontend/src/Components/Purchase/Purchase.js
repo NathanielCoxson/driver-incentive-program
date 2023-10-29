ccad9929037b0ffa44e391c5eae20a3679aca0b8
lanttyc <lanttyc@g.clemson.edu>
2023-10-28 22:25:41 -0400

    add purchase and change shopping cart

--- project/frontend/src/Components/Purchase/Purchase.js
+++ project/frontend/src/Components/Purchase/Purchase.js
@@ -4,9 +4,9 @@ import './Purchase.css';
 function Purchase() {
     // Sample purchase history (replace with your data)
     const purchaseHistory = [
-        { id: 1, date: '2023-03-15', pointsSpent: 50, items: ['Product A', 'Product B'] },
-        { id: 2, date: '2023-03-10', pointsSpent: 30, items: ['Product C'] },
-        { id: 3, date: '2023-03-05', pointsSpent: 70, items: ['Product A', 'Product B', 'Product C'] },
+        { id: 1, date: '2023-03-15', pointsSpent: 50, items: ['Product A', 'Product B'], status: 'Delivered' },
+        { id: 2, date: '2023-03-10', pointsSpent: 30, items: ['Product C'], status: 'Processing' },
+        { id: 3, date: '2023-03-05', pointsSpent: 70, items: ['Product A', 'Product B', 'Product C'], status: 'Processing' },
     ];
 
     return (
@@ -20,6 +20,7 @@ function Purchase() {
                                 <span>Date: {purchase.date}</span>
                                 <span>Points Spent: {purchase.pointsSpent} points</span>
                                 <span>Items: {purchase.items.join(', ')}</span>
+                                <span>Status: {purchase.status}</span> {/* Display the status */}
                             </div>
                         </li>
                     ))}
