784604847860b4a9e83a77763f33c14a90ea5651
lanttyc <lanttyc@g.clemson.edu>
2023-10-31 18:52:25 -0400

    Changing how the radio looks

--- project/frontend/src/Components/Report/SponsorReport.js
+++ project/frontend/src/Components/Report/SponsorReport.js
@@ -21,18 +21,20 @@ function SponsorReport() {
                     <br />
 
                     <p>Select Audit Log Category</p>
-                    <input type="radio" name="set2" id="driverApps" value="option2-1" />
-                    <label for="driverApps">Driver Applications</label>
+                    <div className="radio-inline">
+                        <input type="radio" name="set2" id="driverApps" value="option2-1" />
+                        <label for="driverApps">Driver Applications</label>
 
-                    <input type="radio" name="set2" id="pointChanges" value="option2-2" />
-                    <label for="pointChanges">Point Changes</label>
+                        <input type="radio" name="set2" id="pointChanges" value="option2-2" />
+                        <label for="pointChanges">Point Changes</label>
 
-                    <input type="radio" name="set2" id="pwordChanges" value="option2-3" />
-                    <label for="pwordChanges">Password Changes</label>
+                        <input type="radio" name="set2" id="pwordChanges" value="option2-3" />
+                        <label for="pwordChanges">Password Changes</label>
 
-                    <input type="radio" name="set2" id="loginAttempts" value="option2-4" />
-                    <label for="loginAttempts">Login Attempts</label>
-
+                        <input type="radio" name="set2" id="loginAttempts" value="option2-4" />
+                        <label for="loginAttempts">Login Attempts</label>
+                    </div>
+                    
                     <div className="row">
                         <div className="column-right">
                             <a href="#" className="cta-button">
@@ -64,12 +66,13 @@ function SponsorReport() {
                     <br />
                     <p>Generate the report for all drivers in your organization or a specific driver?</p>
 
-                    <input type="radio" name="set1" id="allDrivers" value="option1-1" />
-                    <label for="allDrivers">All Drivers</label>
+                    <div className="radio-inline">
+                        <input type="radio" name="set1" id="allDrivers" value="option1-1" />
+                        <label for="allDrivers">All Drivers</label>
 
-                    <input type="radio" name="set1" id="indDriver" value="option1-2" />
-                    <label for="indDriver">Individual Driver&nbsp;&nbsp;</label>
-                    <br />
+                        <input type="radio" name="set1" id="indDriver" value="option1-2" />
+                        <label for="indDriver">Individual Driver&nbsp;&nbsp;</label>
+                    </div>
 
                     <label for="driverUsername">Driver Username:</label>
                     <input type="text" id="driverUsername" name="driverUsername" />
