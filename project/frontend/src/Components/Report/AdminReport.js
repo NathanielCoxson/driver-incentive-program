2379cc0ebe24ed7b22cc64ec792d8ab43e1eec76
lanttyc <lanttyc@g.clemson.edu>
2023-10-31 19:58:44 -0400

    Adding the rest of section

--- project/frontend/src/Components/Report/AdminReport.js
+++ project/frontend/src/Components/Report/AdminReport.js
@@ -9,7 +9,16 @@ function AdminReport() {
                 <br />
                 <div className="auditLog">
                     <h2>Audit Log</h2>
+                    {/* ... (Audit Log section) */}
+                </div>
 
+                <div className="sponsorInvoice">
+                    <h2>Sponsor Invoice</h2>
+                    {/* ... (Sponsor Invoice section) */}
+                </div>
+
+                <div className="driverSales">
+                    <h2>Sales by Driver</h2>
                     <p>Select Date Range</p>
                     <label htmlFor="startdatepicker">Start Date:</label>
                     <input type="date" id="startdatepicker" name="startdatepicker" />
@@ -21,10 +30,10 @@ function AdminReport() {
 
                     <p>Generate the report for all sponsors or a specific sponsor?</p>
                     <div className="radio-inline">
-                        <input type="radio" name="set1" id="allSponBox" value="option1-1" />
+                        <input type="radio" name="set4" id="allSponBox" value="option4-1" />
                         <label htmlFor="allSponBox">All Sponsors</label>
 
-                        <input type="radio" name="set1" id="individualSponBox" value="option1-2" />
+                        <input type="radio" name="set4" id="individualSponBox" value="option4-2" />
                         <label htmlFor="individualSponBox">Individual Sponsor&nbsp;&nbsp;</label>
                     </div>
 
@@ -34,24 +43,33 @@ function AdminReport() {
                     <br />
                     <br />
 
-                    <p>Select Audit Log Category</p>
+                    <p>Generate the report for all drivers associated with this sponsor or a specific driver?</p>
                     <div className="radio-inline">
-                        <input type="radio" name="set2" id="driverApps" value="option2-1" />
-                        <label htmlFor="driverApps">Driver Applications</label>
+                        <input type="radio" name="set5" id="allDriverBox" value="option5-1" />
+                        <label htmlFor="allDriverBox">All Drivers</label>
 
-                        <input type="radio" name="set2" id="pointChanges" value="option2-2" />
-                        <label htmlFor="pointChanges">Point Changes</label>
+                        <input type="radio" name="set5" id="individualDriverBox" value="option5-2" />
+                        <label htmlFor="individualDriverBox">Individual Driver&nbsp;&nbsp;</label>
+                    </div>
 
-                        <input type="radio" name="set2" id="pwordChanges" value="option2-3" />
-                        <label htmlFor="pwordChanges">Password Changes</label>
+                    <label htmlFor="indDriverText">Driver Username:</label>
+                    <input type="text" id="indDriverText" name="indDriverUser" />
 
-                        <input type="radio" name="set2" id="loginAttempts" value="option2-4" />
-                        <label htmlFor="loginAttempts">Login Attempts</label>
+                    <br />
+                    <br />
+
+                    <p>Select View Type</p>
+                    <div className="radio-inline">
+                        <input type="radio" name="set6" id="detView" value="option6-1" />
+                        <label htmlFor="detView">Detailed View</label>
+
+                        <input type="radio" name="set6" id="sumView" value="option6-2" />
+                        <label htmlFor="sumView">Summary View</label>
                     </div>
 
                     <div className="row">
                         <div className="column-right">
-                            <a href="#" className="cta-button">View Audit Log</a>
+                            <a href="#" className="cta-button">View Driver Sales Report</a>
                         </div>
                         <div className="column-left">
                             <a href="#" className="cta-button">Download CSV</a>
@@ -59,8 +77,50 @@ function AdminReport() {
                     </div>
                 </div>
 
-                {/* Continue with other sections (sponsorInvoice, driverSales, sponsorSales) similarly */}
+                <div className="sponsorSales">
+                    <h2>Sales by Sponsor</h2>
+                    <p>Select Date Range</p>
+                    <label htmlFor="startdatepicker">Start Date:</label>
+                    <input type="date" id="startdatepicker" name="startdatepicker" />
+                    <label htmlFor="enddatepicker">End Date:</label>
+                    <input type="date" id="enddatepicker" name="enddatepicker" />
 
+                    <br />
+                    <br />
+
+                    <p>Generate the report for all sponsors or a specific sponsor?</p>
+                    <div className="radio-inline">
+                        <input type="radio" name="set7" id="allSponBox" value="option7-1" />
+                        <label htmlFor="allSponBox">All Sponsors</label>
+
+                        <input type="radio" name="set7" id="individualSponBox" value="option7-2" />
+                        <label htmlFor="individualSponBox">Individual Sponsor&nbsp;&nbsp;</label>
+                    </div>
+
+                    <label htmlFor="indSponText">Sponsor Username:</label>
+                    <input type="text" id="indSponText" name="indSponsorUser" />
+
+                    <br />
+                    <br />
+
+                    <p>Select View Type</p>
+                    <div className="radio-inline">
+                        <input type="radio" name="set8" id="detView" value="option8-1" />
+                        <label htmlFor="detView">Detailed View</label>
+
+                        <input type="radio" name="set8" id="sumView" value="option8-2" />
+                        <label htmlFor="sumView">Summary View</label>
+                    </div>
+
+                    <div className="row">
+                        <div className="column-right">
+                            <a href="#" className="cta-button">View Sponsor Sales Report</a>
+                        </div>
+                        <div className="column-left">
+                            <a href="#" className="cta-button">Download CSV</a>
+                        </div>
+                    </div>
+                </div>
             </section>
         </main>
     );
