4fec2ed4f84f0fc353c061a6ffbef871c9dc9873
cfscott11 <116143071+cfscott11@users.noreply.github.com>
2023-10-30 23:18:21 -0400

    Sponsor driver list fixed

--- project/frontend/src/Components/SponsorDriverList/SponsorDriverList.js
+++ project/frontend/src/Components/SponsorDriverList/SponsorDriverList.js
@@ -7,7 +7,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 function SponsorDriverList(){
     const {auth} = useAuth();
 
-    const [drivers, setDrivers] = useState({});
+    const [drivers, setDrivers] = useState([]);
     const navigate = useNavigate();
     const location = useLocation();
 
@@ -17,7 +17,6 @@ function SponsorDriverList(){
         let isMounted = true;
 
         const fetchSponsorDrivers = async () => {
-            console.log(auth?.sponsors[0]?.SponsorName);
             // Make the request
             try {
                 const response = await axios.get("applications/drivers/" + auth?.sponsors[0]?.SponsorName);
@@ -38,12 +37,12 @@ function SponsorDriverList(){
         <section className="hero">
             <h2>Drivers in your Organization</h2>
             <ol className="driver-list">
-                {/* {drivers.map((driver) => (
+                {drivers.map((driver) => (
                     <li>
                         <h3>{driver.Name}</h3>
                         <h3>{driver.Username}</h3>
                     </li>
-                ))} */}
+                ))}
             </ol>
         </section>
     );
