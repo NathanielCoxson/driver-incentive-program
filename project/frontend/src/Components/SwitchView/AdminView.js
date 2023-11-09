2ad37862eee88b283ada8f5df39865b1259fb2cf
lanttyc <lanttyc@g.clemson.edu>
2023-11-09 13:28:05 -0500

    re-routing

--- project/frontend/src/Components/SwitchView/AdminView.js
+++ project/frontend/src/Components/SwitchView/AdminView.js
@@ -1,12 +1,13 @@
 import './SwitchView.css';
+import { Link } from 'react-router-dom';
 
 function AdminView() {
 
     return (<main>
         <section className="hero">
             <h2>View</h2>
-            <Link to="dashboard" className="cta-button">View As Driver</Link>
-            <Link to="dashboard" className="cta-button">View As Sponsor</Link>
+            <Link to="driver_dashboard" className="cta-button">View As Driver</Link>
+            <Link to="sponsor_dashboard" className="cta-button">View As Sponsor</Link>
         </section>
     </main>)
 }
--- project/frontend/src/Components/SwitchView/SponsorView .js
+++ project/frontend/src/Components/SwitchView/SponsorView .js
@@ -1,11 +1,12 @@
 import './SwitchView.css';
+import { Link } from 'react-router-dom';
 
 function SponsorView() {
 
     return (<main>
         <section className="hero">
             <h2>View</h2>
-            <Link to="dashboard" className="cta-button">View As Driver</Link>
+            <Link to="sponsor_dashboard" className="cta-button">View As Driver</Link>
         </section>
     </main>)
 }
