c8ea73b978d00b2e69c7cdc07f786b823805b534
lanttyc <lanttyc@g.clemson.edu>
2023-11-23 17:18:41 -0500

    fix routing

--- project/frontend/src/Components/Dashboard/DriverDashboard.js
+++ project/frontend/src/Components/Dashboard/DriverDashboard.js
@@ -7,7 +7,7 @@ function DriverDashboard() {
         <section className="hero">
             <h2>Welcome to Your Dashboard</h2>
             <p>Welcome to your driver dashboard. Here you can access various features and information.</p>
-            <Link to='/password-reset'>Forgot Password?</Link>
+            <Link to="/dashboard" className="cta-button">Explore</Link>
         </section>
     );
 }
