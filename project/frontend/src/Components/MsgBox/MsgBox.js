1ff8a57af087e14fc26b1c54bd74ca2f07bf9e2a
lanttyc <lanttyc@g.clemson.edu>
2023-11-23 14:36:56 -0500

    Simply MsgBox,js

--- project/frontend/src/Components/MsgBox/MsgBox.js
+++ project/frontend/src/Components/MsgBox/MsgBox.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import "./MsgBox.css";
-import { Link } from 'react-router-dom';
 import useAuth from "../../hooks/useAuth";
 
 function MsgBox() {
@@ -8,60 +7,36 @@ function MsgBox() {
 
     return (
         <div className="hero">
-            {auth?.Role === 'driver' && (
-                <div>
-                    <h1>Messages</h1><br />
-                    <form>
-                        <label htmlFor="recipientEmail">To:</label>
-                        <select id="recipientEmail" name="recipientEmail" required>
+            <h1>Messages</h1><br />
+            <form>
+                <label htmlFor="recipientEmail">To:</label>
+                <select id="recipientEmail" name="recipientEmail" required>
+                    {auth?.Role === 'driver' && (
+                        <>
                             <option value="sponsor1@example.com">Sponsor 1</option>
                             <option value="sponsor2@example.com">Sponsor 2</option>
                             <option value="ithelp@example.com">IT Help</option>
-                        </select>
-                        <br /><br />
-                        <label htmlFor="message">Message:</label>
-                        <textarea id="message" name="message" rows="10" required></textarea>
-                        <br />
-                        <button className="cta-button" type="submit">Send Message</button>
-                    </form>
-                </div>
-            )}
-
-            {auth?.Role === 'sponsor' && (
-                <div>
-                    <h1>Messages</h1><br />
-                    <form>
-                        <label htmlFor="recipientEmail">To:</label>
-                        <select id="recipientEmail" name="recipientEmail" required>
+                        </>
+                    )}
+                    {auth?.Role === 'sponsor' && (
+                        <>
                             <option>All Drivers</option>
                             <option>IT Help</option>
-                        </select>
-                        <br /><br />
-                        <label htmlFor="message">Message:</label>
-                        <textarea id="message" name="message" rows="10" required></textarea>
-                        <br />
-                        <button className="cta-button" type="submit">Send Message</button>
-                    </form>
-                </div>
-            )}
-
-            {auth?.Role === 'admin' && (
-                <div>
-                    <h1>Messages</h1><br />
-                    <form>
-                        <label htmlFor="recipientEmail">To:</label>
-                        <select id="recipientEmail" name="recipientEmail" required>
+                        </>
+                    )}
+                    {auth?.Role === 'admin' && (
+                        <>
                             <option>All Drivers</option>
                             <option>All Sponsors</option>
-                        </select>
-                        <br /><br />
-                        <label htmlFor="message">Message:</label>
-                        <textarea id="message" name="message" rows="10" required></textarea>
-                        <br />
-                        <button className="cta-button" type="submit">Send Message</button>
-                    </form>
-                </div>
-            )}
+                        </>
+                    )}
+                </select>
+                <br /><br />
+                <label htmlFor="message">Message:</label>
+                <textarea id="message" name="message" rows="10" required></textarea>
+                <br />
+                <button className="cta-button" type="submit">Send Message</button>
+            </form>
         </div>
     );
 }
