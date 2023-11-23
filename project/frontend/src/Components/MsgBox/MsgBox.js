e007150a197179820197082d9c72e5d8c5cbdf2e
lanttyc <lanttyc@g.clemson.edu>
2023-11-23 14:25:46 -0500

    Add driver message box

--- project/frontend/src/Components/MsgBox/MsgBox.js
+++ project/frontend/src/Components/MsgBox/MsgBox.js
@@ -1,35 +1,47 @@
-import "./MsgBox.css"
+import React from 'react';
+import "./MsgBox.css";
 import { Link } from 'react-router-dom';
 import useAuth from "../../hooks/useAuth";
 
 function MsgBox() {
     const { auth } = useAuth();
+
     return (
         <div className="hero">
-            <ul>
-                {auth?.Role === 'driver' &&
-                    <>
-                    Here is driver.
-                    </>
-                }
+            {auth?.Role === 'driver' && (
+                <div>
+                    <h1>Messages</h1><br />
+                    <form>
+                        <label htmlFor="recipientEmail">To:</label>
+                        <select id="recipientEmail" name="recipientEmail" required>
+                            <option value="sponsor1@example.com">Sponsor 1</option>
+                            <option value="sponsor2@example.com">Sponsor 2</option>
+                            <option value="ithelp@example.com">IT Help</option>
+                        </select>
+                        <br /><br />
+                        <label htmlFor="message">Message:</label>
+                        <textarea id="message" name="message" rows="10" required></textarea>
+                        <br />
+                        <button className="cta-button" type="submit">Send Message</button>
+                    </form>
+                </div>
+            )}
 
-                {auth?.Role === 'sponsor' &&
-                    <>
-                    Here is sponsor.
-                    </>
-                }
+            {auth?.Role === 'sponsor' && (
+                // Add content for sponsor role if needed
+                <div>
+                    Here is sponsor content.
+                </div>
+            )}
 
-                {auth?.Role === 'admin' &&
-                    <>
-                    Here is admin
-                    </>
-                }
-            </ul>
+            {auth?.Role === 'admin' && (
+                // Add content for admin role if needed
+                <div>
+                    Here is admin content.
+                </div>
+            )}
         </div>
     );
 }
 
 export default MsgBox;
-
-
-
